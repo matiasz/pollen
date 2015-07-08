d125ca49064b9d58088fde99316e28f7c6431575
Matthew Butterick <mb@mbtype.com>
2015-07-08 00:02:59 -0700

    test case

--- test-rerequire/run-test-case.rkt
+++ test-rerequire/run-test-case.rkt
@@ -1,8 +1,8 @@
 #lang racket/base
 (require pollen/render sugar pollen/cache rackunit racket/file)
 
+(require pollen/render sugar pollen/cache rackunit racket/file)
 (define (touch ps) (display-to-file (file->string ps) ps #:exists 'replace))
-
 (define (make-dr arg) (format "#lang racket/base
 (provide do)
 (define (do) ~v)" arg))
@@ -12,12 +12,9 @@
   (when (file-exists? "pollen.cache") (delete-file "pollen.cache"))
   (display-to-file (make-dr "first-dr") dr #:exists 'replace)
   (check-equal? (render (->complete-path "one.html.pp")) "first-dr")
-  (check-equal? (file-or-directory-modify-seconds "one.html.pp") (hash-ref (cache-ref "one.html.pp") 'mod-time))
-  (check-equal? (render (->complete-path "two.html.pp")) "first-dr")
-  (check-equal? (file-or-directory-modify-seconds "two.html.pp") (hash-ref (cache-ref "two.html.pp") 'mod-time))
+  ;(check-equal? (render (->complete-path "two.html.pp")) "first-dr")
   (display-to-file (make-dr "second-dr") dr #:exists 'replace)  
-  (reset-cache)
   (check-equal? (render (->complete-path "one.html.pp")) "second-dr")
-  (check-equal? (render (->complete-path "two.html.pp")) "second-dr")
+  ;(check-equal? (render (->complete-path "two.html.pp")) "second-dr")
   
   )
