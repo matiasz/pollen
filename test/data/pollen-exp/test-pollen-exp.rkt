086ac9ce3704374c2ea6e0425ad632fdfc371060
Matthew Butterick <mb@mbtype.com>
2015-11-02 23:24:52 -0800

    try this

--- test/data/pollen-exp/test-pollen-exp.rkt
+++ test/data/pollen-exp/test-pollen-exp.rkt
@@ -2,11 +2,6 @@
 (require racket/string)
 
 (define (proc)
-  (apply string-join (string-split ∆string-append{foo bar zam}) ∆'{X}))
+    (apply string-join (string-split ◊string-append{foo bar zam}) ◊'{X}))
 
-(display (proc))
-
-(module test racket/base
-  ;; Racket 6.0 does not support test-omit-paths
-  ;; and it chokes on this file, so suppress the testing
-  )
+(proc)
