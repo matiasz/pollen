805d7185a93c4d80b798bcfff335569d92eb29f0
Matthew Butterick <mb@mbtype.com>
2015-11-02 23:13:16 -0800

    adjust at est

--- test/data/pollen-exp/test-pollen-exp.rkt
+++ test/data/pollen-exp/test-pollen-exp.rkt
@@ -5,3 +5,8 @@
   (apply string-join (string-split ∆string-append{foo bar zam}) ∆'{X}))
 
 (display (proc))
+
+(module test racket/base
+  ;; Racket 6.0 does not support test-omit-paths
+  ;; and it chokes on this file, so suppress the testing
+  )
